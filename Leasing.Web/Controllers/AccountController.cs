db28ca0d6ab8238641347039e3c945a52a936004
Tito <tito.parra4@@hotmail.com>
2019-11-12 11:55:24 +0100

    Data Table

--- Leasing.Web/Controllers/AccountController.cs
+++ Leasing.Web/Controllers/AccountController.cs
@@ -351,7 +351,7 @@ namespace Leasing.Web.Controllers
                     "ResetPassword",
                     "Account",
                     new { token = myToken }, protocol: HttpContext.Request.Scheme);
-                _mailHelper.SendMail(model.Email, "MyLeasing Password Reset", $"<h1>MyLeasing Password Reset</h1>" +
+                _mailHelper.SendMail(model.Email, "Leasing Password Reset", $"<h1>MyLeasing Password Reset</h1>" +
                     $"To reset the password click in this link:</br></br>" +
                     $"<a href = \"{link}\">Reset Password</a>");
                 ViewBag.Message = "The instructions to recover your password has been sent to email.";
